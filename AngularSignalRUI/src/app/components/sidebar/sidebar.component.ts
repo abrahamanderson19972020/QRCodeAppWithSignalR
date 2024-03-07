be8e47687f5ef37648f7e76fcbebd94df8ff1f2a
abrahamanderson19972020 <abraham.anderson83@gmail.com>
2024-03-07 22:40:51 +0100

    Initial Layout is added

---
+++ AngularSignalRUI/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+
+declare const $: any;
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: "/dashboard", title: "Dashboard", icon: "dashboard", class: "" },
+  { path: "/about", title: "About", icon: "book", class: "" },
+  { path: "/menu", title: "Menu", icon: "content_paste", class: "" },
+  {
+    path: "/categories",
+    title: "Categories",
+    icon: "library_books",
+    class: "",
+  },
+  { path: "/products", title: "Products", icon: "bubble_chart", class: "" },
+  { path: "/messages", title: "Messages", icon: "message", class: "" },
+  {
+    path: "/rezervations",
+    title: "Rezervations",
+    icon: "notifications",
+    class: "",
+  },
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"],
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+  }
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  }
+}
